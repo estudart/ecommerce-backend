8e9480fec0c1e20b43f4b1acc7e552135873417a
estudart <erico.studartdf@gmail.com>
2024-03-29 23:03:05 -0300

    post product route

--- controllers/productControllers.js
+++ controllers/productControllers.js
@@ -1,5 +1,16 @@
+import Product from "../backend/models/product.js";
+
 export const getProducts = async (req, res) => {
   res.status(200).json({
     message: "All products",
+  });
+};
+
+// Create new Product => /api/v1/admin/products
+export const newProduct = async (req, res) => {
+  const product = await Product.create(req.body);
+
+  res.status(200).json({
+    product,
   });
 };
--- routes/products.js
+++ routes/products.js
@@ -1,7 +1,8 @@
-import { getProducts } from "../controllers/productControllers.js";
+import { getProducts, newProduct } from "../controllers/productControllers.js";
 import express from "express";
 const router = express.Router();
 
 router.route("/products").get(getProducts);
+router.route("/admin/products").post(newProduct);
 
 export default router;
